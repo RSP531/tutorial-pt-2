88ff3b296cc11433cf4fce837930694ec79d3398
Rob Peschke <rspesc31@gmail.com>
2019-10-15 11:41:13 -0500

    added bashrc file

---
+++ .bashrc
@@ -0,0 +1,5 @@
+# Start the SSH agent
+eval `ssh-agent -s`
+# Add the SSH key
+ssh-add
+
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     deploy: {
         production: {
         user: 'ubuntu',
-        host: 'ec2-52-209-166-225.eu-west-1.compute.amazonaws.com',
-        key: '~/.ssh/tutorial-2.pem',
+        host: 'ec2-18-188-224-244.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/robTest.pem',
         ref: 'origin/master',
-        repo: 'git@github.com:roberttod/tutorial-pt-2.git',
-        path: '/home/ubuntu/tutorial-2',
+        repo: 'git@github.com:roberttod/tutorial-pt-2.git', 
+        path: '/home/ubuntu/tutorial-pt-2',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
