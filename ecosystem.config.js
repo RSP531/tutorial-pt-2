008b385c039d31eed7bdee9f38bdbc6cf66b8fde
Rob Peschke <rspesc31@gmail.com>
2019-10-15 12:10:12 -0500

    adjusted eco config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: 'tutorial-2',
+        name: 'tutorial-pt-2',
         script: './index.js'
     }],
     deploy: {
@@ -9,7 +9,7 @@ module.exports = {
         host: 'ec2-18-188-224-244.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/robTest.pem',
         ref: 'origin/master',
-        repo: 'git@github.com:roberttod/tutorial-pt-2.git', 
+        repo: 'git@github.com:RSP531/tutorial-pt-2.git', 
         path: '/home/ubuntu/tutorial-pt-2',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
